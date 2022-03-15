bf0475c09caefa7e418a2ca29f0ea536e4a58c9d
Mat <silencekrakens@gmail.com>
2022-03-15 09:57:06 +0100

    Update

---
+++ src/main/java/com/metaxcrew/actitickets/Commands/TicketsCommand.java
@@ -0,0 +1,18 @@
+package com.metaxcrew.actitickets.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TicketsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+        
+
+        }
+    return true;}
+}
